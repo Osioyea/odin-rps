6ea3cff373e9ae7b8f52948dfb6b90845446d502
Osioyea <131928742+Osioyea@users.noreply.github.com>
2023-06-16 13:03:27 -0500

    Add logic to determine the winner of a match.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,39 @@
-function getComputerChoice() {
-  let computerThink = Math.random() * 3;
-  let computerChoice = "";
-  computerThink = parseInt(computerThink);
-  switch(computerThink){
+function getCpuChoice() {
+  let cpuThink = Math.random() * 3;
+  let cpuChoice = "";
+  cpuThink = parseInt(cpuThink);
+  switch(cpuThink){
     case 0:
-      computerChoice = "Rock";
+      cpuChoice = "rock";
       break;
     case 1 :
-      computerChoice = "Paper";
+      cpuChoice = "paper";
       break;
     case 2:
-      computerChoice = "Scissors";
+      cpuChoice = "scissors";
       break;
   }
-  return computerChoice;
+  return cpuChoice;
 }
+
+function playMatch(humanChoice, cpuChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  let result;
+  if(humanChoice == cpuChoice) {
+    result = 0;
+  } else if(humanChoice == "rock") {
+    result = cpuChoice == "paper" ? -1 : 1;
+  } else if(humanChoice == "paper") {
+    result = cpuChoice == "scissors" ? -1 : 1;
+  } else if(humanChoice == "scissors") {
+    result = cpuChoice == "rock" ? -1 : 1;
+  }
+  return result;
+}
+
+
+let playerChoice = getCpuChoice();
+let computerChoice = getCpuChoice();
+console.log(playerChoice);
+console.log(computerChoice);
+console.log(playMatch(playerChoice, computerChoice));
